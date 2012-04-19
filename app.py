e99cc9147ad7ea345e180419824d195fd688e1b8
Justin Woodbridge <jwoodbridge@me.com>
2012-04-18 20:27:46 -0400

    go go go go go go go go go go

--- app.py
+++ app.py
@@ -6,74 +6,125 @@ import database
 from database import Person
 from sqlalchemy import func
 from locale import setlocale, LC_ALL
+import daemon
+import os
 
 GOAL = 400000
 stripe.api_key = "6gK51e6bKVHTjrDdTb5nOUYuiy35nu9o"
+
 define('port', default=8000)
+define('pid_dir', default='pids/')
+
 setlocale(LC_ALL, '')
+
 
 # Handles /
 class SiteHandler(tornado.web.RequestHandler):
-	def get(self):
-		amount_sum = database.session.query(func.sum(Person.amount)).scalar()
-		if not amount_sum:
-			amount_sum = 0
+    def get(self):
+        amount_sum = database.session.query(func.sum(Person.amount)).scalar()
+        if not amount_sum:
+            amount_sum = 0
 
-		total = (GOAL - amount_sum) / 100
-		self.render('index.html', total=total)
+        total = (GOAL - amount_sum) / 100
+        self.render('index.html', total=total)
 
 # Handles /payments
 class PaymentsHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.redirect('/')
+    def get(self):
+        self.redirect('/')
 
-	def post(self):
-		token = self.get_argument('stripeToken')
-		amount = int(self.get_argument('amount')) * 100
-		name = self.get_argument('name')
-		currency = "usd"
-		description = "Donation to west coast economics x-term."
+    def post(self):
+        token = self.get_argument('stripeToken')
+        amount = int(self.get_argument('amount')) * 100
+        name = self.get_argument('name')
+        email = self.get_argument('email')
+        currency = "usd"
+        description = "Donation to west coast economics x-term."
 
-		charge = stripe.Charge.create(
-			amount=amount,
-			card=token,
-			description=description,
-			currency=currency
-		)
+        charge = stripe.Charge.create(
+            amount=amount,
+            card=token,
+            description=description,
+            currency=currency
+        )
 
-		p = Person()
-		p.name = name
-		p.stripe_charge_id = charge.id
-		p.amount = amount
+        p = Person()
+        p.name = name
+        p.email = email
+        p.stripe_charge_id = charge.id
+        p.amount = amount
 
-		database.session.add(p)
-		database.session.commit()
+        database.session.add(p)
+        database.session.commit()
 
-		self.redirect('/receipts/%s' % p.stripe_charge_id)
+        self.redirect('/receipts/%s' % p.stripe_charge_id)
 
 # Handles /receipts
 class RecieptsHandler(tornado.web.RequestHandler):
-	def get(self, person_id):
-		query = database.session.query(Person).filter(Person.stripe_charge_id == person_id)
-		person = query.first()
+    def get(self, person_id):
+        query = database.session.query(Person).filter(Person.stripe_charge_id == person_id)
+        person = query.first()
 
-		self.render('receipt.html', person=person)
+        self.render('receipt.html', person=person)
 
+# Handles /receipts
 class StackHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('stack.html')
+    def get(self):
+        self.render('stack.html')
+
+# Handles /admin
+class AdminController(tornado.web.RequestHandler):
+    def get(self):
+
+        people = database.session.query(Person).all()
+        amount = database.session.query(func.sum(Person.amount)).scalar() / 100
+        self.render('admin.html', people=people, amount=amount)
 
 application = tornado.web.Application([
-	(r"/", SiteHandler),
-	(r"/payments", PaymentsHandler),
-	(r"/receipts/(.+)", RecieptsHandler),
-	(r"/stack", StackHandler)
 ], debug=True, template_path='templates/', static_path='static/')
 
+
+class App(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", SiteHandler),
+            (r"/payments", PaymentsHandler),
+            (r"/receipts/(.+)", RecieptsHandler),
+            (r"/stack", StackHandler),
+            (r"/admin", AdminController)
+        ]
+
+        settings = dict(
+            template_path='templates/',
+            static_path='static/'
+        )
+
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
+    tornado.options.parse_command_line()
+    production = os.environ.get('XTERM_ENV')
 
-	application.listen(options.port)
-	print "Booting up the x-term site on http://127.0.0.1:%d" % options.port
+    if production:
+        log = open('logs/' + 'tornado.' + str(options.port) + '.log', 'a+')
+        ctx = daemon.DaemonContext(stdout=log, stderr=log, working_directory='.')
+        ctx.open()
 
-	tornado.ioloop.IOLoop.instance().start()
+    App().listen(options.port)
+    print "Booting up the x-term site on http://127.0.0.1:%d" % options.port
+
+
+    if not os.path.exists(options.pid_dir):
+        os.mkdir(options.pid_dir)
+
+    if production:
+        pid = os.getpid()
+        pid_file_path = os.path.join(options.pid_dir, 'app.pid')
+        pid_file = open(pid_file_path, 'wb')
+        pid_file.seek(0)
+        pid_file.truncate(0)
+        pid_file.write(str(pid))
+        pid_file.close()
+
+    tornado.ioloop.IOLoop.instance().start()
---
+++ fabfile.py
@@ -0,0 +1,38 @@
+from fabric.api import local, run, cd, env
+import os
+
+env.user = 'justin'
+env.hosts = ['justinwoodbridge.com']
+env.code_dir = '/var/www/xterm'
+env.pid_dir = os.path.join(env.code_dir, 'pids')
+
+def setup():
+  with cd('/var/www'):
+    os.mkdir('xterm')
+
+def stop():
+  # If the app wasn't running, that's okay.
+  env.warn_only = True
+  with cd(env.code_dir):
+    run('kill -HUP `cat %s/app.pid`' % env.pid_dir, pty=True)
+
+def start():
+  with cd(env.code_dir):
+    run('XTERM_ENV=production python app.py')
+
+
+def install_deps():
+  with cd(env.code_dir):
+    run('sudo pip install -r requirements.txt')
+
+def deploy():
+  local('git push')
+  with cd(env.code_dir):
+    run('git pull')
+    # install_deps()
+    stop()
+    start()
+
+def tail():
+  with cd(os.path.join(env.code_dir, 'logs')):
+    run('tail -f *.log', pty=True)
